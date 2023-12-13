aa309de0476088ef8704dea5756de80fb0ce2aa0
Christer Miroy <chrstrzm.mry@gmail.com>
2023-12-14 00:02:41 +0800

    update hotel api done

--- routes/hotels.js
+++ routes/hotels.js
@@ -15,4 +15,19 @@ router.post("/new", async (req, res) => {
     }
 });
 
+// update
+router.put("/update/:id", async (req, res) => {
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        );
+        
+        res.status(200).json(updatedHotel);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 export default router;
