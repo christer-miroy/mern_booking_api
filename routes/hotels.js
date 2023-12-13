42f7d70c5a4574f2e4a3fb921dec750202472b07
Christer Miroy <chrstrzm.mry@gmail.com>
2023-12-14 00:10:33 +0800

    delete hotel, get all hotels, get single hotel api done

--- routes/hotels.js
+++ routes/hotels.js
@@ -23,8 +23,38 @@ router.put("/update/:id", async (req, res) => {
             { $set: req.body },
             { new: true }
         );
-        
+
         res.status(200).json(updatedHotel);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// delete
+router.delete("/delete/:id", async (req, res) => {
+    try {
+        await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has been deleted");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// get hotel
+router.get("/:id", async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// all hotels
+router.get("/", async (req, res) => {
+    try {
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
     } catch (error) {
         res.status(500).json(error);
     }
